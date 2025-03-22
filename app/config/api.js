a6637fae4554b2ddc07365fd79b175ba7cf5c99e
bitnevis8 <bitnevis@yahoo.com>
2025-03-22 08:44:48 +0330

    upgrade

--- app/config/api.js
+++ app/config/api.js
@@ -2,16 +2,24 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const API_BASE_URL = isDevelopment
   ? 'http://localhost:3000'
-  : 'https://aryafoulad-api-api.parandx.com';
+  : 'https://aryafoulad-api.parandx.com';
 
 export const API_ENDPOINTS = {
   unitLocations: {
-    base: `${API_BASE_URL}/aryafoulad-api/unit-locations`,
-    getAll: `${API_BASE_URL}/aryafoulad-api/unit-locations`,
-    getById: (id) => `${API_BASE_URL}/aryafoulad-api/unit-locations/${id}`,
-    create: `${API_BASE_URL}/aryafoulad-api/unit-locations/create`,
-    update: (id) => `${API_BASE_URL}/aryafoulad-api/unit-locations/update/${id}`,
-    delete: (id) => `${API_BASE_URL}/aryafoulad-api/unit-locations/delete/${id}`,
-    default: `${API_BASE_URL}/aryafoulad-api/unit-locations/default`,
+    base: `${API_BASE_URL}/aryafoulad/unit-locations`,
+    getAll: `${API_BASE_URL}/aryafoulad/unit-locations`,
+    getById: (id) => `${API_BASE_URL}/aryafoulad/unit-locations/${id}`,
+    create: `${API_BASE_URL}/aryafoulad/unit-locations/create`,
+    update: (id) => `${API_BASE_URL}/aryafoulad/unit-locations/update/${id}`,
+    delete: (id) => `${API_BASE_URL}/aryafoulad/unit-locations/delete/${id}`,
+    default: `${API_BASE_URL}/aryafoulad/unit-locations/default`,
+  },
+  missionOrders: {
+    base: `${API_BASE_URL}/aryafoulad/mission-orders`,
+    getAll: `${API_BASE_URL}/aryafoulad/mission-orders`,
+    getById: (id) => `${API_BASE_URL}/aryafoulad/mission-orders/${id}`,
+    create: `${API_BASE_URL}/aryafoulad/mission-orders/create`,
+    update: (id) => `${API_BASE_URL}/aryafoulad/mission-orders/update/${id}`,
+    delete: (id) => `${API_BASE_URL}/aryafoulad/mission-orders/delete/${id}`,
   },
 }; 
--- app/dashboard/settings/unit-locations/[id]/page.js
+++ app/dashboard/settings/unit-locations/[id]/page.js
@@ -1,13 +1,11 @@
 "use client";
 
-import { use } from "react";
 import UnitLocationDetails from "@/app/components/unitLocation/UnitLocationDetails/UnitLocationDetails";
 
 export default function UnitLocationDetailsPage({ params }) {
-  const { id } = use(params);
   return (
     <div className="p-6">
-      <UnitLocationDetails id={id} />
+      <UnitLocationDetails id={params.id} />
     </div>
   );
 }
